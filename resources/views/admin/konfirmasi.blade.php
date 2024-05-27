a30988c31d9740b7aeb381656aa72132c490b7d5
rengganabakta <rengganabakta@gmail.com>
2024-05-28 00:03:15 +0700

    add konfirmasi admin

---
+++ resources/views/admin/konfirmasi.blade.php
@@ -0,0 +1,4 @@
+@extends('includes.sidebar')
+@section('admin-content')
+    <h1 class="content_admin">test</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Models\Admin;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
 
 Route::get('/', [UserController::class, 'dashboard'])->name('dashboard');
@@ -10,3 +12,4 @@ Route::get('/informasi', [UserController::class, 'informasi'])->name('informasi'
 Route::get('/pinjam', [UserController::class, 'pinjam'])->name('pinjam');
 Route::get('/profil', [UserController::class, 'profil'])->name('profil');
 Route::get('/riwayat', [UserController::class, 'riwayat'])->name('riwayat');
+Route::get('/konfirmasi', [AdminController::class,'konfirmasi'])->name('konfirmasi');
